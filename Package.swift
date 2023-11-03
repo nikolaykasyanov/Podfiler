2385e311f91b82a307e93ef9021fa79102703c7c
Mostafa Amer <1731640+mosamer@users.noreply.github.com>
2023-11-03 12:18:45 +0100

    Upgrade target platform and swift-tools-version

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Podfiler",
+    platforms: [.macOS(.v13)],
     products: [
         .executable(name: "podfiler", targets: ["Podfiler"]),
         .library(name: "PodfilerKit", targets: ["PodfilerKit"]),
